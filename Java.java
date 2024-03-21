be9949cb87177596d21c50ed97cc6a0d9cda3b0d
andikaekasuandi <andikaekasuandi27@gmail.com>
2024-03-21 16:52:56 +0700

    latihanvar

---
+++ Java.java
@@ -0,0 +1,24 @@
+package com.mycompany.java;
+
+/**
+ *
+ * @author oitco
+ */
+public class Java {
+ public static void main(String[] args){ 
+        
+        int JariJari;
+        double keliling;
+        final double PI=3.14;
+        
+        JariJari = 30;
+        
+        keliling= 2*PI*JariJari;
+        System.out.println("keliling lingkaran = "+keliling);
+        System.out.println("=============================");
+        }
+    
+}
+
+   
+    
