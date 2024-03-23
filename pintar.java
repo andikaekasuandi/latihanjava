02a57e13c030d85ba03aafb0044ea77d7f83ec7b
andikaekasuandi <andikaekasuandi27@gmail.com>
2024-03-23 14:52:58 +0700

    belajar.pintar

--- pintar.java
+++ pintar.java
@@ -5,10 +5,9 @@ package encapsulation;
  * @author oitco
  */
 public class pintar {
-   
-   public static void main(String[]args){
-   belajar panggil = new belajar();
-   System.out.println("panggil x "+panggil.x);
-   System.out.println("panggil x "+panggil.y);
+    public static void main(String[]args){
+        belajar panggil = new belajar();
+        System.out.println("Panggil x : "+panggil.x);
+        System.out.println("Panggil y : "+panggil.y);
 }
 }
