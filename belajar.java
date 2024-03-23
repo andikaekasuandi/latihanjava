9729589fa78e8fe127b2686abbd84e4ae52f0f36
andikaekasuandi <andikaekasuandi27@gmail.com>
2024-03-23 14:26:00 +0700

    latihanjava02

--- belajar.java
+++ belajar.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package encapsulation;
 
 /**
  *
  * @author oitco
  */
-class belajar 
-{
-        public String x = "pintar";
-        private String y = "java";
+public class belajar {
+    public String x = "pintar";
+    public String y = "java";
 
 }
